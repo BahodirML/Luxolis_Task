0b772cee7d72ef8f96a885da1386599d564f8272
Bahodir Alayorov <116503420+BahodirML@users.noreply.github.com>
2024-07-06 18:13:06 +0900

    Update humans.py

--- humans.py
+++ humans.py
@@ -5,22 +5,22 @@ from mtcnn import MTCNN
 from frames import frames
 from frames import extract_frames
 
-
+#detecting humans with their faces
 def highlight_faces(frames):
-    # Initialize MTCNN face detector
+    
     detector = MTCNN()
 
-    # Create directory if it doesn't exist
+    # Create a directory 
     os.makedirs('humans', exist_ok=True)
 
     for i, frame in enumerate(frames):
         height, width, channels = frame.shape
 
-        # Detecting faces
+        # Detect faces
         results = detector.detect_faces(frame)
 
         for result in results:
-            if result['confidence'] > 0.5:  # You can adjust the confidence threshold
+            if result['confidence'] > 0.5: 
                 x, y, w, h = result['box']
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
@@ -29,7 +29,7 @@ def highlight_faces(frames):
 # Extract frames
 frames = extract_frames('human.mp4')
 
-# Highlight faces in the frames
+# Highlight faces 
 highlight_faces(frames)
 
 print("Processing complete. Check the highlighted directory for output images.")
